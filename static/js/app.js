99621c74c225462ef6dcc0e414dc4c522354e619
eaveltre21189 <e.a.veltre@gmail.com>
2023-06-12 21:18:36 -0400

    updated app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,60 @@
+function createMap(restaurantMarkers) { 
+
+// Create the map base layers.
+var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+// Create a baseMaps object.
+var baseMaps = {
+  "Street Map": street
+};
+
+// Create an overlay object to hold our overlay.
+var overlayMaps = {
+  Restaurants: restaurantMarkers
+};      
+
+// // Clear previous map  
+// document.getElementById("map").innerHTML = "";
+
+// Create our map, giving it the streetmap and earthquakes layers to display on load.
+  var myMap = new L.map("map", {
+    center: [40.75, -74],
+    zoom: 11.5,
+    layers: [street, restaurantMarkers]
+  });
+
+    // Create a layer control.
+    // Pass it our baseMaps and overlayMaps.
+    // Add the layer control to the map.
+    L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+    }).addTo(myMap);  
+  };
+
+ function createMarkers(response) {
+
+      var restaurants = response;
+    
+      var restaurantMarkers = [];
+      
+      // Looping through the cities array, create one marker for each city, bind a popup containing its name and population, and add it to the map.         
+      
+      for (var i = 0; i < restaurants.length; i++) {
+        var restaurant = restaurants[i];
+
+        console.log(restaurant)
+        
+        var restaurantMarker = L.marker([restaurant.latitude, restaurant.longitude])
+          .bindPopup(`<h1>${restaurant.name}</h1> <hr> <h3>Restaurant Type: ${restaurant.restaurant_type}</h3>`);
+        
+        restaurantMarkers.push(restaurantMarker);
+        }
+      // createMap(L.layerGroup(restaurantMarkers));
+      restaurantMarkers.addTo(myMap);
+    }; 
+
 // Load the API data
 var queryURL = "http://127.0.0.1:5000/restaurants"
 
@@ -82,62 +139,51 @@ d3.json(queryURL).then(function(data) {
             }
           );
 
-      // Mapping the restaurant
-      // Create the base layers.
-      var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      });
-
-      var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-        attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-      });
-
-      var restaurants = new L.LayerGroup();
-
-      // Create a baseMaps object.
-      var baseMaps = {
-          "Street Map": street,
-          "Topographic Map": topo
+        // Prepare data for the food review chart
+        var foodReviews = filteredRestaurants.map(restaurant => restaurant.food_review);
+        var foodReviewColors = ['rgba(255, 99, 132, 0.5)', 'rgba(54, 162, 235, 0.5)', 'rgba(255, 205, 86, 0.5)', 'rgba(75, 192, 192, 0.5)', 'rgba(153, 102, 255, 0.5)', 'rgba(255, 159, 64, 0.5)'];
+        var foodReviewChartData = {
+          labels: filteredRestaurants.map(restaurant => restaurant.name),
+          datasets: [{
+            label: "Food Reviews",
+            data: foodReviews,
+            backgroundColor: foodReviewColors.slice(0, filteredRestaurants.length),
+            borderColor: foodReviewColors.slice(0, filteredRestaurants.length).map(color => color.replace('0.5', '1')),
+            borderWidth: 1
+          }]
         };
-        
-      // Create an overlay object to hold our overlay.
-      var overlayMaps = {
-        Restaurants: restaurants
-      };      
-
-      // // Clear previous map
-      // var myMap = document.getElementById("map").getContext("2d");
-      // if (myMap) {
-      //   myMap.destroy();
-      //   };      
-
-        // Create our map, giving it the streetmap and earthquakes layers to display on load.
-          var myMap = new L.map("map", {
-            center: [40.75, -74],
-            zoom: 11.5,
-            layers: [street, restaurants]
-          });
-        
-            // Create a layer control.
-            // Pass it our baseMaps and overlayMaps.
-            // Add the layer control to the map.
-            L.control.layers(baseMaps, overlayMaps, {
-              collapsed: false
-            }).addTo(myMap); 
-
-            // Looping through the cities array, create one marker for each city, bind a popup containing its name and population, and add it to the map.
-            for (var i = 0; i < filteredRestaurants.length; i++) {
-              var restaurant = filteredRestaurants[i];
-              console.log(restaurant)
-              L.marker([restaurant.latitude, restaurant.longitude])
-                .bindPopup(`<h1>${restaurant.name}</h1> <hr> <h3>Restaurant Type: ${restaurant.restaurant_type}</h3>`)
-                .addTo(restaurants);
+        // Clear previous food review chart
+        var foodReviewChart = document.getElementById("foodReviewChart").getContext("2d");
+        if (window.foodReviewChartInstance) {
+          window.foodReviewChartInstance.destroy();
+        }
+        // Create the food review chart
+        window.foodReviewChartInstance = new Chart(foodReviewChart, {
+          type: "bar",
+          data: foodReviewChartData,
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true,
+                max: 10,
+                stepSize: 1
               }
+            },
+            plugins: {
+              legend: {
+                display: false
+              },
+              title: {
+                display: true,
+                text: "Food Reviews by Restaurant"
+              }
+            }
+          }
+        });
 
-            restaurants.addTo(myMap);
+        createMarkers(filteredRestaurants);
       
       } else {
-        
         // Display a message if no restaurants of the selected type are found
         restaurantList.html("<p>No restaurants found for the selected type.</p>");
         }
@@ -158,60 +204,3 @@ d3.json(queryURL).then(function(data) {
   }).catch(function(error) {
     console.log("Error loading JSON data:", error);
   });
-
-// // Mapping the restaurant
-  
-// // Store our API endpoint as queryUrl.
-// var queryURL = "http://127.0.0.1:5000/restaurants"
-
-// // Perform a GET request to the query URL/
-// d3.json(queryURL).then(function(data) {
-
-// // Create the base layers.
-// var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-// });
-
-// var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-//   attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-// });
-
-// var restaurants = new L.LayerGroup();
-
-// // Create a baseMaps object.
-// var baseMaps = {
-//     "Street Map": street,
-//     "Topographic Map": topo
-//   };
-  
-// // Create an overlay object to hold our overlay.
-// var overlayMaps = {
-//   Restaurants: restaurants
-// };
-
-// // Create our map, giving it the streetmap and earthquakes layers to display on load.
-// var myMap = L.map("map", {
-//   center: [40.71, -74],
-//   zoom: 7,
-//   layers: [street, restaurants]
-// });
-      
-//   // Create a layer control.
-// // Pass it our baseMaps and overlayMaps.
-// // Add the layer control to the map.
-// L.control.layers(baseMaps, overlayMaps, {
-//   collapsed: false
-// }).addTo(myMap); 
-
-// // Looping through the cities array, create one marker for each city, bind a popup containing its name and population, and add it to the map.
-// for (var i = 0; i < data.length; i++) {
-// var restaurant = data[i];
-// console.log(restaurant)
-// L.marker([restaurant.latitude, restaurant.longitude])
-// .bindPopup(`<h1>${restaurant.name}</h1> <hr> <h3>Restaurant Type: ${restaurant.restaurant_type}</h3>`)
-// .addTo(restaurants);
-// }
-
-// restaurants.addTo(myMap);
-
-// });
